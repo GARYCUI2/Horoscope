ce4e1bc43438760af50a8fca0b54504f193fb9c7
Lindsay Wilhelm <lindsaywilhelm@Lindsays.local>
2022-02-14 14:52:18 -0500

    adds zodiac images to home page and renders aries component on click

--- react-front-end/src/components/Home.jsx
+++ react-front-end/src/components/Home.jsx
@@ -1,25 +1,130 @@
-import React from "react";
+import React, { useState } from "react";
+import Aries from "./zodiac_signs/Aries";
 
 function Home() {
+  const [show, setShow] = useState(true);
+  const [active, setActive] = useState(false);
+  const onClick = () => {
+    setActive(true);
+    if (setActive === false) {
+      setShow(false);
+    }
+  }
+
+  const onDoubleClick = () => {
+    setActive(false);
+  }
+
+
   return (
     <div className="home">
-      <div class="container">
-        <div class="row align-items-center my-5">
-          <div class="col-lg-7">
+      <div className="container">
+        <div className="row align-items-center my-5">
+          <div className="col-lg-5">
+            <h1 className="font-weight-light">Horoscope</h1>
+            <p>
+              Welcome.
+            </p>
+            <form>
+              <label>Enter Your Birthday</label>
+              <input placeholder="DD/MM/YYYY" />
+            </form>
+            <label>Or Choose Your Sign</label>
+            { show ? (
+            <div className="col-lg-7">
+            <section onDoubleClick={onDoubleClick}>
+            <img
+              className="img-fluid rounded mb-4 mb-lg-0 hover-shadow"
+              name="aries"
+              src="/aries.jpeg"
+              alt="Aries Symbol"
+              height="300"
+              width="200"
+              onClick={onClick}
+              
+            />
+            { active ? <Aries /> : null }
+            
+            </section>
             <img
               class="img-fluid rounded mb-4 mb-lg-0"
-              src="http://placehold.it/900x400"
-              alt=""
+              src="/taurus.jpeg"
+              alt="Taurus Symbol"
+              height="300"
+              width="200"
             />
-          </div>
-          <div class="col-lg-5">
-            <h1 class="font-weight-light">Home</h1>
-            <p>
-              Lorem Ipsum is simply dummy text of the printing and typesetting
-              industry. Lorem Ipsum has been the industry's standard dummy text
-              ever since the 1500s, when an unknown printer took a galley of
-              type and scrambled it to make a type specimen book.
-            </p>
+             <img
+              className="img-fluid rounded mb-4 mb-lg-0"
+              src="/gemini.jpeg"
+              alt="Gemini Symbol"
+              height="300"
+              width="200"
+            />
+             <img
+              className="img-fluid rounded mb-4 mb-lg-0"
+              src="/cancer.jpeg"
+              alt="Cancer Symbol"
+              height="300"
+              width="200"
+            />
+             <img
+              className="img-fluid rounded mb-4 mb-lg-0"
+              src="/leo.jpeg"
+              alt="Leo Symbol"
+              height="300"
+              width="200"
+            />
+             <img
+              className="img-fluid rounded mb-4 mb-lg-0"
+              src="/virgo.jpeg"
+              alt="Virgo Symbol"
+              height="300"
+              width="200"
+            />
+             <img
+              className="img-fluid rounded mb-4 mb-lg-0"
+              src="/libra.jpeg"
+              alt="Libra Symbol"
+              height="300"
+              width="200"
+            />
+             <img
+              className="img-fluid rounded mb-4 mb-lg-0"
+              src="/scorpio.jpeg"
+              alt="Scorpio Symbol"
+              height="300"
+              width="200"
+            />
+             <img
+              className="img-fluid rounded mb-4 mb-lg-0"
+              src="/sagittarius.jpeg"
+              alt="Sagittarius Symbol"
+              height="300"
+              width="200"
+            />
+             <img
+              className="img-fluid rounded mb-4 mb-lg-0"
+              src="/capricorn.jpeg"
+              alt="Capricorn Symbol"
+              height="300"
+              width="200"
+            />
+             <img
+              className="img-fluid rounded mb-4 mb-lg-0"
+              src="/aquarius.jpeg"
+              alt="Aquarius Symbol"
+              height="300"
+              width="200"
+            />
+             <img
+              className="img-fluid rounded mb-4 mb-lg-0"
+              src="/pisces.jpeg"
+              alt="Pisces Symbol"
+              height="300"
+              width="200"
+            />
+          </div>) : null}
+          
           </div>
         </div>
       </div>
---
+++ react-front-end/src/components/zodiac_signs/Aries.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Aries() {
+  return (
+    <p>This is the Aries Sign</p>
+  )
+}
+
+export default Aries
