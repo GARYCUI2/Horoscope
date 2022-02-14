6347301e7c0f5bdb6053fc836c776b29c7ab59c2
GARY <garycui222@gmail.com>
2022-02-14 14:19:12 -0500

    create horoscope route only for test dummy data.

---
+++ express-back-end/routes/horoscope.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const router = express.Router();
+const axios = require('axios');
+
+const horoscopeHelper = require("../db_service/horoscopeHandler");
+
+module.exports = (db) => {
+  router.get("/", 
+  (req,res) => {
+    horoscopeHelper.getAllHoroscope(db)
+    .then(dbRes => {
+      res.send(dbRes);
+    })
+    .catch(err => {
+      res.status(500)
+        .json({error:err.message})
+    })
+  })
+
+  router.get("/:sign", 
+  (req,res) => {
+    let sign = req.params.sign;
+    const URL = `https://aztro.sameerkumar.website/?sign=${sign}&day=today`
+
+    axios.post(URL)
+    .then(result => {
+        res.send(result.data);
+    })
+    .catch(err => {
+      res.status(500)
+        .json({error:err.message})
+    })
+  })
+  
+  return router;
+}
