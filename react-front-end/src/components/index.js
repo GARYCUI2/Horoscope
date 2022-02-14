54d168901dd9f4f642ae939fa1009a084e96208f
Hailan Wang <hailan6257@gmail.com>
2022-02-14 09:09:29 -0500

    market --> shop

--- react-front-end/src/components/index.js
+++ react-front-end/src/components/index.js
@@ -2,5 +2,5 @@ export { default as Navigation } from "./Navigation";
 export { default as Footer } from "./Footer";
 export { default as Home } from "./Home";
 export { default as Cart } from "./Cart";
-export { default as Market } from "./Market";
+export { default as Shop } from "./Shop";
 export { default as Orders } from "./Orders";
