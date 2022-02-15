eeb9a7da7669c37ee753bd8d16f80be43471f369
GARY <garycui222@gmail.com>
2022-02-14 23:55:22 -0500

    add helper function for edit product

--- express-back-end/db_service/productsHandler.js
+++ express-back-end/db_service/productsHandler.js
@@ -20,6 +20,16 @@ const getProductsBySign = function(db) {
     // .catch(console.error("error running in get all horoscope!!!!"))
 };
 
+const editProductsBySign = function(db) {
+  // let quertString = `SELECT * FROM astrologies`;
+  const productsPath = '../json/recommend-product.json';
+  return Promise.resolve(getJsonData(productsPath));
+    //db;
+    // .query(quertString)
+    // .then(res => res.row[0])
+    // .catch(console.error("error running in get all horoscope!!!!"))
+};
+
 module.exports = {
   getAllProducts,
   getProductsBySign
