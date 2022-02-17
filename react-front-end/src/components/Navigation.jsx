3d12d25a4f2ef4633d4c1563f23a08c26043e0fc
Lindsay Wilhelm <lindsaywilhelm@Lindsays.local>
2022-02-17 15:41:40 -0500

    adds better styling to home page and adds recommended item that links to the shop

--- react-front-end/src/components/Navigation.jsx
+++ react-front-end/src/components/Navigation.jsx
@@ -7,7 +7,7 @@ function Navigation(props) {
       <nav className="navbar navbar-expand navbar-dark bg-dark">
         <div className="container">
           <NavLink className="navbar-brand" to="/">
-          Astrology
+          Luck Star Astrology
           </NavLink>
           <div>
             <ul className="navbar-nav ml-auto">
