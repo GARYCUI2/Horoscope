66e3b2f296a463f40f541ee5f16c18dba04168ec
Lindsay Wilhelm <lindsaywilhelm@Lindsays.local>
2022-02-20 16:14:09 -0500

    sets the state for updating products and categories

--- react-front-end/src/components/Admin.jsx
+++ react-front-end/src/components/Admin.jsx
@@ -1,221 +1,213 @@
-// import React, { useState, useEffect } from "react";
-// import axios from 'axios';
-
-// function Admin(props) {
-//   const [item, setItem] = useState({});
-//   const [category, setCategory] = useState({});
-//   const [values, setValues] = useState({ 
-//     category_id: '',
-//     id: '',
-//     description: "",
-//     img_url: "",
-//     name: "",
-//     price: "",
-//     quantity: ''
-//    });
-
-//   useEffect(() => {
-//     axios.get(`/api/categories`)
-//     .then((res) => {
-//      const categoryObject = res.data
-//      setCategory(categoryObject[0])
-//     }).then(
-//     )
-//   },[]);
-
-//   useEffect(() => {
-//     axios.get(`/api/products`)
-//     .then((res) => {
-//       setItem(res.data[0]);
-//     })
-//   },[]);
-
-//   if (!category) return null;
-//   if (!item) return null;
-
-//   if (!item) return null;
-
-//   const handleChange = (e) => {
-//     setValues({ ...values, [e.target.name]: e.target.value })
-//   }
-
-// const handleSubmit = (e) => {
-//   e.preventDefault();
-//   const productData = {
-//     // category_id: values.category_id,
-//     id: values.id,
-//     description: values.description,
-//     img_url: values.img_url,
-//     name: values.name,
-//     price: values.price,
-//     quantity: values.quantity
-//   }
-//   axios.post('/api/products', productData)
-//         .then((res) => {
-//           setValues({
-//             id: values.id,
-//             description: values.description,
-//             img_url: values.img_url,
-//             name: values.name,
-//             price: values.price,
-//             quantity: values.quantity
-//           })
-//           console.log(res.status)
-//           console.log(res.data.token)
-//         }).catch((err) => {
-//           if (err.response) {
-//             console.log(err.response);
-//             console.log("server responded");
-//           } else if (err.request) {
-//             console.log("network error");
-//           } else {
-//             console.log(err);
-//           }
-//         })
-//       }
-
-//       console.log(item)
-//       console.log(category)
-
-//       const handleDelete = (id) => {
-        
-//       }
-
-//   return (
-//     <div className="about">
-//       <div className="container">
-//         <div className="row align-items-center my-5">
-//           <div className="col-lg-7">
-//           <h1 className="font-weight-light text-center">Admin</h1>
-//           <h2>Categories:</h2>
-//           <table className="text-center">
-//             <thead>
-//               <tr>
-//                 <td>Category ID</td>
-//                 <td>Category Name</td>
-//               </tr>
-//             </thead>
-//             <tbody>
-//                  <tr key={category.id}>
-//                    <td>{category.id}</td>
-//                  <td>{category.category_name}</td>
-//                </tr>
-//             </tbody>
-//           </table>
-//           <br />
-//           <h2>Products:</h2>
-//           <table className="text-center">
-//             <thead>
-//               <tr>
-//                 <td>Name</td>
-//                 <td>Image</td>
-//                 <td>Price</td>
-//                 <td>Quantity</td>
-//                 <td>Edit</td>
-//                 <td>Delete</td>
-//               </tr>
-//             </thead>
-//             <tbody>
-//                  <tr>
-//                  <td>{item.name}</td>
-//                  <td><img src={item.img_url} alt=""/></td>
-//                  <td>${Number(item.price).toFixed(2)}</td>
-//                  <td>{item.quantity}</td>
-//                  <td><button>Edit</button></td>
-//                  <td><button>Delete</button></td>
-//                </tr>
-//             </tbody>
-//           </table>
-//           <br />
-//           <hr />
-//           <h2>Add A Product</h2>
-//           <form onSubmit={handleSubmit}>
-//             <div className="form-group">
-//             {/* <label>Category ID:</label>
-//             <input type='text' className='form-control' name="category_id" value={values.category_id} onChange={handleChange} /> */}
-//             <label>ID:</label>
-//             <input type='text' className='form-control' name="id" value={values.id} onChange={handleChange} />
-//             <label>Description:</label>
-//             <input type='text' className='form-control' name="description"  value={values.description} onChange={handleChange} />
-//             <label>Image:</label>
-//             <input type='text' className='form-control' name="img_url" value={values.img_url} onChange={handleChange} />
-//             <label>Name:</label>
-//             <input type='text' className='form-control' name="name" value={values.name} onChange={handleChange} />
-//             <label>Price:</label>
-//             <input type='text' className='form-control' name="price" value={values.price} onChange={handleChange} />
-//             <label>Quantity:</label>
-//             <input type='text' className='form-control' name="quantity" value={values.quantity} onChange={handleChange} />
-//             </div>
-//             <button type="submit">Add</button>
-//           </form>
-//           </div>
-//           <div className="col-lg-5">
-//           </div>
-//         </div>
-//       </div>
-//     </div>
-//   );
-//   }
-
-// export default Admin;
-
-import React, {useState, useEffect} from "react";
-import { useParams } from "react-router-dom";
+import React, { useState, useEffect } from "react";
 import axios from 'axios';
 
-function Admin(props) {
-  const params = useParams();
-  const [item, setItem] = useState({});
-  const {onAdd} = props;
-  // console.log(params.id);
+function Admin() {
+  const [item, setItem] = useState();
+  const [category, setCategory] = useState();
+  const [catValue, setCatValue] = useState({
+    id: '',
+    category_name: ''
+  })
+  const [values, setValues] = useState({ 
+    category_id: '',
+    id: '',
+    description: '',
+    img_url: '',
+    name: '',
+    price: '',
+    quantity: '',
+   });
 
   useEffect(() => {
-    axios.get(`/api/products/${params.id}`)
-    .then((item) => {
-      // console.log(item.data);
-      setItem(item.data[0]);
-    });
+    axios.get(`/api/categories`)
+    .then((res) => {
+      console.log(res)
+     setCategory(res.data)
+    })
+    .catch((err) => {
+      console.error('An error occurred: ', err)
+    })
   },[]);
-//  console.log(item);
-  if (!item) return null;
- 
 
-  return (
-    <div className="shop">
-    <div className="container" >
-      <div className="row align-items-center my-5">
-         <React.Fragment key = {item.id}>
-          <div className="col-lg-7">
-          <img
-            className="img-fluid rounded mb-4 mb-lg-0"
-            src={item.img_url}
-            alt={item.name}
-          />
-        </div>
-        <div className="col-lg-5">
-          <h2 className="font-weight-light" >
-            {item.name}
-            </h2>
-          <p >
-          ${Number(item.price).toFixed(2)}
-          </p>
-          <p >
-          Only {item.quantity} left in stock
-          </p>
-          <p >
-          Description:{item.description}
-          </p>
-          <section className="appointment__card-right">
-        <section className="appointment__actions">
-          <button onClick={() => onAdd(item)}>Add To Cart</button>
-        </section>
-        </section>
-        </div>
-        </React.Fragment>
-      </div>
-    </div>
-    </div>
-  );
+  useEffect(() => {
+    axios.get(`/api/products`)
+    .then((res) => {
+      console.log(res)
+      setItem(res.data);
+      setValues(res.data);
+    })
+    .catch((err) => {
+      console.error('An error occurred: ', err)
+    })
+  },[]);
+
+  if (!category) return null;
+  if (!item) return null;
+
+  const handleProductChange = (e) => {
+    setValues({ ...values, [e.target.name]: e.target.value })
+  }
+
+  const handleCategoryChange = (e) => {
+    setCatValue({ ...catValue, [e.target.name]: e.target.value })
+  }
+
+  const handleCategorySubmit = (e) => {
+    e.preventDefault();
+  const categoryData = {
+    id: catValue.id,
+    category_name: catValue.category_name
+  }
+  axios.post('/api/categories', categoryData)
+        .then(() => {
+          setCategory([...category, categoryData])
+        })
+        .catch((err) => {
+          if (err.response) {
+            console.log(err.response);
+            console.log("server responded");
+          } else if (err.request) {
+            console.log("network error");
+          } else {
+            console.log(err);
+          }
+        })
+  }
+
+
+const handleProductSubmit = (e) => {
+  e.preventDefault();
+  const productData = {
+    id: values.id,
+    name: values.name,
+    description: values.description,
+    category: values.category,
+    price: values.price,
+    img_url: values.img_url,
+    quantity: values.quantity
+  }
+  axios.post('/api/products', productData)
+        .then(() => {
+          setItem([...item, productData])
+        })
+        .catch((err) => {
+          if (err.response) {
+            console.log(err.response);
+            console.log("server responded");
+          } else if (err.request) {
+            console.log("network error");
+          } else {
+            console.log(err);
+          }
+        })
 }
 
+      const handleDelete = (id) => {
+        axios.delete(`/api/products/${id}`)
+              .then()
+      }
+
+  return (
+    <div className="about">
+      <div className="container">
+        <div className="row align-items-center my-5">
+          <div className="col-lg-7">
+          <h1 className="font-weight-light text-center">Admin</h1>
+          <h2>Categories:</h2>
+          <table className="table">
+            <tr>
+            <th scope="col">Category ID</th>
+              <th scope="col">Category Name</th>
+              <th scope="col">Edit</th>
+                <th scope="col">Delete</th>
+                </tr>
+                {
+                category.map(category => 
+                <tbody>
+                  <tr>
+                <th scope="row">{category.id}</th>
+                <td key={category.id}>{category.category_name}</td>
+                 <td><button>Edit</button></td>
+                 <td><button onClick={handleDelete}>Delete</button></td>
+                 </tr>
+                 </tbody> )
+                } 
+          </table> 
+          <h2>Add A Category</h2>
+          <form onSubmit={handleCategorySubmit}>
+            <div className="form-group">
+            <label>Category ID:</label>
+            <input type='text' className='form-control' name="id"  onChange={handleCategoryChange} />
+            <label>Category Name:</label>
+            <input type='text' className='form-control' name="category_name" onChange={handleCategoryChange} />
+            </div>
+            <button type="submit">Add</button>
+            </form>
+            <hr />
+          <br />
+          <h2>Products:</h2>
+          <table className="table">
+            <tr>
+            <th scope="col">ID</th>
+              <th scope="col">Name</th>
+              <th scope="col">Category ID</th>
+                <th scope="col">Image</th>
+                <th scope="col">Price</th>
+                <th scope="col">Quantity</th>
+                <th scope="col">Edit</th>
+                <th scope="col">Delete</th>
+                </tr>
+            
+                 
+                {
+                item.map(item => 
+                <tbody>
+                  <tr>
+                <th scope="row">{item.id}</th>
+                <td key={item.id}>{item.name}</td>
+                <td>{item.category_id}</td>
+                <td><img src={item.img_url} alt=""/></td>
+                 <td>${Number(item.price).toFixed(2)}</td>
+                 <td>{item.quantity}</td>
+                 <td><button>Edit</button></td>
+                 <td><button onClick={handleDelete}>Delete</button></td>
+                 </tr>
+                 </tbody> )
+                } 
+
+          </table> 
+
+          
+          <br />
+          <hr />
+          <h2>Add A Product</h2>
+          <form onSubmit={handleProductSubmit}>
+            <div className="form-group">
+            <label>Category ID:</label>
+            <input type='text' className='form-control' name="category" onChange={handleProductChange} />
+            <label>ID:</label>
+            <input type='text' className='form-control' name="id" onChange={handleProductChange} />
+            <label>Description:</label>
+            <input type='text' className='form-control' name="description"  onChange={handleProductChange} />
+            <label>Image:</label>
+            <input type='text' className='form-control' name="img_url"  onChange={handleProductChange} />
+            <label>Name:</label>
+            <input type='text' className='form-control' name="name" onChange={handleProductChange} />
+            <label>Price:</label>
+            <input type='text' className='form-control' name="price" onChange={handleProductChange} />
+            <label>Quantity:</label>
+            <input type='text' className='form-control' name="quantity" onChange={handleProductChange} />
+            </div>
+            <button type="submit">Add</button>
+          </form>
+          </div>
+          <div className="col-lg-5">
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+  }
+
 export default Admin;
+
