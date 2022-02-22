e73bf7c597580a6d4d242e110b528f748e38c44f
Hailan Wang <hailan6257@gmail.com>
2022-02-22 17:03:16 -0500

    Update order and successs message

--- react-front-end/src/components/Success.jsx
+++ react-front-end/src/components/Success.jsx
@@ -6,7 +6,8 @@ function Success(props) {
   return (
     <aside className="block col-7">
     <h1 className="text-danger">Congrats!</h1>
-    <h2 className="text-info">Stripe has successfully processed your payment</h2>
+    <h2 className="text-info">Stripe has successfully processed your payment.</h2>
+    <h2 className="text-info">Checkout out your email to track the order.</h2>
   </aside>
   );
 }
